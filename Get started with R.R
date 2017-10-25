f7c53c86cd2770f149b9155c4ed1cbba063911f4
nguyen-m <nguyen.m.fr@gmail.com>
2017-10-25 17:19:59 +0200

    update the lesson w2

--- Get started with R.R
+++ Get started with R.R
@@ -27,4 +27,35 @@ fac1 <- factor(c("yes", "no", "no", "yes")) #levels: no yes
 ### create a factor with defined levels
 fac2 <- factor(c("yes", "no", "no", "yes"), levels = c("yes", "no"))
 
-# Names
+# Remove NA values
+
+x <- c(1, 2, NA, 4, 5, NA)
+bad <- is.na(x)
+bad
+x[!bad] ## return goods values (not NA values) of x
+
+y <- c("a", NA, "b", NA, "c", "d")
+
+good <- complete.cases(x,y)
+good
+
+
+#Subsetting
+x <- c("a", "b", "c", "d", "c")
+### get the third element of x
+x[3]
+
+## get the first four elements of the vector x
+x[1:4]
+
+### get the elements that greater the "b"
+x[ x> "b"]
+
+
+# Subsetting lists
+x <- list(foo = 1:4, bar = 0.6)
+### get the element by name
+x$foo
+### partial matching
+x$b
+x[["b", exact = FALSE]]
